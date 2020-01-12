dcad45f31f215422416366dce1f065c9382efaaa
osakani <imiyako26@gmail.com>
2020-01-12 20:56:02 +0900

    v..v

---
+++ iroiro.js
@@ -0,0 +1,41 @@
+
+var worlds = {
+  'Elemental': [
+    'Aegis',
+    'Atomos',
+    'Carbuncle',
+    'Garuda',
+    'Gungnir',
+    'Kujata',
+    'Ramuh',
+    'Tonberry',
+    'Typhon',
+    'Unicorn'
+  ],
+  'Gaia': [
+    'Alexander',
+    'Bahamut',
+    'Durandal',
+    'Fenrir',
+    'Ifrit',
+    'Ridill',
+    'Tiamat',
+    'Ultima',
+    'Valefor',
+    'Yojimbo',
+    'Zeromus'
+  ],
+  'Mana': [
+    'Anima',
+    'Asura',
+    'Belias',
+    'Chocobo',
+    'Hades',
+    'Ixion',
+    'Mandragora',
+    'Masamune',
+    'Pandaemonium',
+    'Shinryu',
+    'Titan'
+  ]
+};
